30942c8e23c2b577dd96ff7cf884e0198403cc34
tellawiabdullah86 <tellawiabdullah86@gmail.com>
2025-08-12 10:51:24 +0300

    Solve bottom nav bar size

--- lib/common/dashboard/bottomNavigationItem.dart
+++ lib/common/dashboard/bottomNavigationItem.dart
@@ -4,7 +4,6 @@ import 'package:masjidhub/theme/colors.dart';
 import 'package:masjidhub/utils/concaveDecoration.dart';
 import 'package:provider/provider.dart';
 import 'package:masjidhub/provider/bottom_bar_provider.dart';
-import 'package:sizer/sizer.dart';
 
 class BottomNavigationItem extends StatelessWidget {
   final int index;
@@ -27,79 +26,77 @@ class BottomNavigationItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     bool _isSelected = index == currentIndex;
-    bool _isQiblaIcon = index == 1;
-    double _iconContainerLength = 8.5.h;
     final state = Provider.of<BottomBarProvider>(context, listen: true);
-
-    return GestureDetector(
-      behavior: HitTestBehavior.translucent,
-      onTap: () => onItemPressed(index),
-      child: Stack(
-        children: [
-          Container(
-            width: 120,
-            height: state.isNavBarVisible ? _iconContainerLength + 25 : 0,
-            child: Column(
-              children: [
-                Expanded(
-                  child: Container(
-                    width: _iconContainerLength - 2,
-                    height: _iconContainerLength + 25,
-                    margin: EdgeInsets.only(bottom: 10),
-                    decoration: _isSelected
-                        ? ConcaveDecoration(
-                            depth: 9,
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(20)),
-                            colors: innerConcaveShadow,
-                            size: Size(_iconContainerLength,
-                                _iconContainerLength - 5),
-                          )
-                        : BoxDecoration(
-                            boxShadow: tertiaryShadow,
-                            color: CustomColors.solitude,
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-                    child: Center(
-                      child: Icon(
-                        icon,
-                        size: _isQiblaIcon ? 40 : 30,
-                        color: _isSelected
-                            ? Theme.of(context).colorScheme.secondary
-                            : CustomColors.mischka,
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 20),
+      child: GestureDetector(
+        behavior: HitTestBehavior.translucent,
+        onTap: () => onItemPressed(index),
+        child: Stack(
+          children: [
+            Visibility(
+              visible: state.isNavBarVisible,
+              child: Column(
+                children: [
+                  Flexible(
+                    child: Container(
+                      height: 70,
+                      width: 70,
+                      margin: EdgeInsets.only(bottom: 10),
+                      decoration: _isSelected
+                          ? ConcaveDecoration(
+                              depth: 9,
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(20)),
+                              colors: innerConcaveShadow,
+                              size: Size(70, 70),
+                            )
+                          : BoxDecoration(
+                              boxShadow: tertiaryShadow,
+                              color: CustomColors.solitude,
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                      child: Center(
+                        child: Icon(
+                          icon,
+                          size: 30,
+                          color: _isSelected
+                              ? Theme.of(context).colorScheme.secondary
+                              : CustomColors.mischka,
+                        ),
                       ),
                     ),
                   ),
-                ),
-                Text(
-                  label.toUpperCase(),
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    letterSpacing: 1.3,
-                    fontSize: 14.0,
-                    color: _isSelected
-                        ? Theme.of(context).colorScheme.secondary
-                        : CustomColors.mischka,
+                  Text(
+                    label.toUpperCase(),
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      letterSpacing: 1.3,
+                      fontSize: 14.0,
+                      color: _isSelected
+                          ? Theme.of(context).colorScheme.secondary
+                          : CustomColors.mischka,
+                    ),
                   ),
-                ),
-              ],
-            ),
-          ),
-          Visibility(
-            visible: isNewFeature,
-            child: Container(
-              width: 15,
-              height: 15,
-              margin: EdgeInsets.fromLTRB(30, 10, 0, 0),
-              decoration: BoxDecoration(
-                boxShadow: shadowTesbihDot,
-                shape: BoxShape.rectangle,
-                borderRadius: BorderRadius.circular(10),
-                gradient: CustomColors.primary180,
+                ],
               ),
             ),
-          ),
-        ],
+            Visibility(
+              visible: isNewFeature,
+              child: Container(
+                width: 15,
+                height: 15,
+                margin: EdgeInsets.fromLTRB(30, 10, 0, 0),
+                decoration: BoxDecoration(
+                  boxShadow: shadowTesbihDot,
+                  shape: BoxShape.rectangle,
+                  borderRadius: BorderRadius.circular(10),
+                  gradient: CustomColors.primary180,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
